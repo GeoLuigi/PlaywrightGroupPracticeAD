295ca9b0803aedab780f91b7972661a44ffb6db5
Jorge Hurtado <lhgjorge@gmail.com>
2024-01-10 08:57:35 -0400

    ECA-38 test case refactored

--- tests/topsWomen.test.js
+++ tests/topsWomen.test.js
@@ -1,19 +1,19 @@
 import { test, expect } from '@playwright/test'
 import { TopsWomenPage } from '../pages/topsWomenPage'
-import { url } from 'inspector'
+
 
 test('ECA-38 | Verify the correct addition of one filter', async ({ page }) => {
 
-    const topsWomenPage = new TopsWomenPage(page)
-    const filterSubtitle = topsWomenPage.filterSubtitle
+    const topWomenPage = new TopsWomenPage(page)
+    const url = topWomenPage.url
 
-    await page.goto(topsWomenPage.url)
-    await topsWomenPage.clickOnCategoryTab()
-    await topsWomenPage.clickOnJacketsFilter()
+    await page.goto(url)
+    await topWomenPage.clickOnCategoryTab()
+    await topWomenPage.clickOnJacketsFilter()
 
-    const subtitleElement = page.getByRole('tab', { name: filterSubtitle })
+    const subtitleElement = page.getByRole('tab', { name: topWomenPage.filterSubtitle })
     const subtitleText = await subtitleElement.innerText()
 
-    expect(page.url()).toBe(topsWomenPage.url + '?cat=23', 'URL is not correct')
+    expect(page.url()).toBe(url + '?cat=23', 'URL is not correct')
     expect(subtitleText).toContain("Now Shopping by")
 })
