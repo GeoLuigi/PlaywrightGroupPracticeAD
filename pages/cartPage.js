278ec87baec8ea7af205a59bb749c034904d68d5
NicolasRiv24 <152893064+NicolasRiv24@users.noreply.github.com>
2024-01-11 10:17:39 -0400

    Before test creation

---
+++ pages/cartPage.js
@@ -0,0 +1,8 @@
+import { cartLocators } from "../locators/cartLocators";
+
+export class CartPage {
+    constructor(page){
+        this.page = page;
+        this.customerLoginLocator = loginLocators.customerLoginLocator;
+       
+    }
